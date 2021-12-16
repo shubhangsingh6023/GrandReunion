a425f29d3af16c9e1e6c882136c776b58c15e7cf
shubhangsingh6023 <SHUBHANGSINGH2002@GMAIL.COM>
2021-12-16 17:36:46 +0530

    Dashboard

--- dashboard.php
+++ dashboard.php
@@ -10,96 +10,105 @@
 <body>
 <!-- partial:index.partial.html -->
 <div class="container">
-  <div id="logo"><h1 class="logo">hulu</h1>
+  <!-- <div id="logo"><h1 class="logo">hulu</h1>
     <div class="CTA">
       <h1>Get $10</h1>
-      </div>
-  </div>
+    </div>
+  </div> -->
   <div class="leftbox">
     <nav>
       <a id="profile" class="active"><i class="fa fa-user"></i></a>
-      <a id="payment"><i class="fa fa-credit-card"></i></a>
-      <a id="subscription"><i class="fa fa-tv"></i></a>
-      <a id="privacy"><i class="fa fa-tasks"></i></a>
-      <a id="settings"><i class="fa fa-cog"></i></a>
+      <a id="payment"><i class="fas fa-briefcase"></i></a>
+      <a id="subscription"><i class="fas fa-graduation-cap"></i></a>
+      <a id="privacy"><i class="fas fa-syringe"></i></a>
+      <a id="settings"><i class="fas fa-bed"></i></a>
     </nav>
   </div>
   <div class="rightbox">
     <div class="profile">
       <h1>Personal Info</h1>
       <h2>Full Name</h2>
-      <p>Julie Park <button class="btn">update</button></p>
-      <h2>Birthday</h2>
-      <p>July 21</p>
-      <h2>Gender</h2>
-      <p>Female</p>
+      <p>Julie Park</p>
       <h2>Email</h2>
-      <p>example@example.com <button class="btn">update</button></p>
-      <h2>Password </h2>
-      <p>••••••• <button class="btn">Change</button></p>
+      <p>abc@gmail.com</p>
+      <h2>Address</h2>
+      <p>a b c</p>
+      <h2>City</h2>
+      <p>Kolkata</p>
+      <h2>State</h2>
+      <p>West Bengal</p>
+      <h2>Country</h2>
+      <p>India</p>
+      <h2>Zipcode</h2>
+      <p>700114</p>
+      <h2>Mobile</h2>
+      <p>9876543210</p>
+      <h2>DOB</h2>
+      <p>2021-12-31</p>
     </div>
 
     <div class="payment noshow">
-      <h1>Payment Info</h1>
-      <h2>Payment Method</h2>
-      <p>Mastercard •••• •••• •••• 0000 <button class="btn">update</button></p>
-      <h2>Billing Address</h2>
-      <p>1234 Example Ave | Seattle, WA <button class="btn">change</button></p>
-      <h2>Zipcode</h2>
-      <p>999000</p>
-      <h2>Billing History</h2>
-      <p>2018<button class="btn">view</button></p>
-      <h2>Redeem Gift Subscription </h2>
-      <p><input type="text" placeholder="Enter Gift Code"></input> <button class="btn">Redeem</button></p>
+      <h1>Work Experience</h1>
+      <h2>Industry</h2>
+      <p>IT</p>
+      <h2>Profession</h2>
+      <p>Developer</p>
+      <h2>Organisation</h2>
+      <p>Google</p>
+      <h2>Designation</h2>
+      <p>Manager</p>
+      <h2>Work Address</h2>
+      <p>a b c</p>
+      <h2>Work City</h2>
+      <p>Kolkata</p>
+      <h2>Work State</h2>
+      <p>West Bengal</p>
+      <h2>Work Country</h2>
+      <p>India</p>
+      <h2>Work Zipcode</h2>
+      <p>700119</p>
     </div>
 
     <div class="subscription noshow">
-      <h1>Your Subscription</h1>
-      <h2>Payment Date</h2>
-      <p>05-15-2018 <button class="btn">pay now</button></p>
-      <h2>Your Next Charge</h2>
-      <p>$8.48<span> includes tax</span></p>
-      <h2>Hulu Base Plan</h2>
-      <p>Limited Commercials <button class="btn">change plan</button></p>
-      <h2>Add-ons</h2>
-      <p>None <button class="btn">manage</button></p>
-      <h2>Monthly Recurring Total </h2>
-      <p>$7.99/month</p>
+      <h1>Nostalgic Section</h1>
+      <h2>Roll No</h2>
+      <p>20AB123456</p>
+      <h2>Degree</h2>
+      <p>B Tech</p>
+      <h2>Department</h2>
+      <p>Mechanical Engineering</p>
+      <h2>Hall</h2>
+      <p>RK</p>
+      <h2>Year of Graduation</h2>
+      <p>2024</p>
+      <h2>Involvement</h2>
+      <p>SAC</p>
+      <h2>Hobbies</h2>
+      <p>Sleep</p>
     </div>
 
     <div class="privacy noshow">
-      <h1>Privacy Settings</h1>
-      <h2>Manage Email Notifications<button class="btn">manage</button></h2>
-      <p></p>
-      <h2>Manage Privacy Settings<button class="btn">manage</button></h2>
-      <p></p>
-      <h2>View Terms of Use <button class="btn">view</button></h2>
-      <p></p>
-      <h2>Personalize Ad Experience <button class="btn">update</button></h2>
-      <p></p>
-      <h2>Protect Your Account <button class="btn">protect</button></h2>
-      <p></p>
+      <h1>Covid Info</h1>
+      <h2>Vaccination Status</h2>
+      <p>Pending</p>
+      <h2>Able to get vaccinated by 10th Jan</h2>
+      <p>Yes</p>
     </div>
  <div class="settings noshow">
-      <h1>Account Settings</h1>
-      <h2>Sync Watchlist to My Stuff<button class="btn">sync</button></h2>
-      <p></p>
-      <h2>Hold Your Subscription<button class="btn">hold</button></h2>
-      <p></p>
-      <h2>Cancel Your Subscription <button class="btn">cancel</button></h2>
-      <p></p>
-      <h2>Your Devices <button class="btn">Manage Devices</button></h2>
-      <p></p>
-      <h2>Referrals <button class="btn">get $10</button></h2>
-   <p></p>
+      <h1>Accomodation</h1>
+      <h2>Marital Status</h2>
+      <p>No</p>
+      <h2>Accompaniments</h2>
+      <p>1</p>
+      <h2>Guest House</h2>
+      <p>TGH</p>
+      <h2>Total Cost</h2>
+      <p>Rs 15500</p>
     </div>
 
   </div>
 </div>
 
-<footer>
-  <p>made by <a href="https://codepen.io/juliepark"> julie</a> ♡
-</footer>
 <!-- partial -->
   <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script><script  src="./script.js"></script>
 
