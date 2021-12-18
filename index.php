0adb7c5c787d634bdeaf1aac6b482405ab1bb29b
Umang-IIT <73515380+Umang-IIT@users.noreply.github.com>
2021-12-18 15:14:14 +0530

    corrented path of stylesheet in index page

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,300'>
 <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:400,700,300'>
 <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css'>
-<link rel="stylesheet" href="./style.css">
+<link rel="stylesheet" href="./css/style.css">
 
 </head>
 <body>
