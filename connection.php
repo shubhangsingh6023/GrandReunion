43b391382e43c288aa6dc760f14101a913df2f97
Umang-IIT <73515380+Umang-IIT@users.noreply.github.com>
2021-12-22 15:49:05 +0530

    added details of remote db

--- connection.php
+++ connection.php
@@ -1,9 +1,15 @@
 <?php
 
-    $DB_HOST = "localhost";
-    $DB_NAME = "aam";
-    $DB_USER = "root";
-    $DB_PASS = "";
+    // $DB_HOST = "localhost";
+    // $DB_NAME = "aam";
+    // $DB_USER = "root";
+    // $DB_PASS = "";
+
+    $DB_HOST = "db4free.net:3306";
+    $DB_NAME = "grandreunion";
+    $DB_USER = "umangsingla617";
+    $DB_PASS = "12345678";
+
     /*
     $DB_HOST = "172.17.0.8";
     $database = "aam";
