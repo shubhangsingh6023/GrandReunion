bddea90506f93099ef14b55894820e82cdad6b36
puravi-238 <kajolsahu238@gmail.com>
2021-12-22 23:27:44 +0530

    aaded db

--- connection.php
+++ connection.php
@@ -1,10 +1,16 @@
 <?php
 
+$DB_HOST = "https://db4free.net/";
+$DB_NAME = "grandreunion238";
+$DB_USER = "puravi";
+$DB_PASS = "f.aKv_qp4aTVGFC";
+
+/*
     $DB_HOST = "localhost";
     $DB_NAME = "aam";
     $DB_USER = "root";
     $DB_PASS = "";
-    /*
+    
     $DB_HOST = "172.17.0.8";
     $database = "aam";
     $DB_USER = "root";
